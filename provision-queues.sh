ebd964c7ab15463425a765ecb9dab1917f9b774e
Sven Neuhaus <neuhaus@tyntec.com>
2017-09-30 16:12:57 +0200

    use curl -d

--- provision-queues.sh
+++ provision-queues.sh
@@ -2,12 +2,13 @@
 
 curl -X POST 'http://localhost:8080/queue?name=local&description=show+up+in+person'
 curl -X POST 'http://localhost:8080/queue?name=online&description=call'
-curl -X POST http://localhost:8080/queue/local?who=mario
-curl -X POST http://localhost:8080/queue/local?who=luigi
-curl -X POST http://localhost:8080/queue/local?who=peach
-curl -X POST http://localhost:8080/queue/local?who=toad
-curl -X POST http://localhost:8080/queue/online?who=toad
-curl -X POST http://localhost:8080/queue/online?who=mario
+curl -X POST http://localhost:8080/queue/local -d who=mario
+curl -X POST http://localhost:8080/queue/local -d who=luigi
+curl -X POST http://localhost:8080/queue/local -d who=peach
+curl -X POST http://localhost:8080/queue/local -d who=toad
+curl -X POST http://localhost:8080/queue/online -d who=toad
+curl -X POST http://localhost:8080/queue/online -d who=mario
+curl -X POST http://localhost:8080/queue/online -d who=wario
 
 if type -f pretty-json 2> /dev/null ; then
 	curl http://localhost:8080/queue | pretty-json
