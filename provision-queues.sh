887a5ce4a99e63478e9b3333e3de762721cd701a
Sven Neuhaus <neuhaus@tyntec.com>
2017-09-30 16:18:58 +0200

    use phone number for queue name local

--- provision-queues.sh
+++ provision-queues.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -X POST 'http://localhost:8080/queue?name=local&description=show+up+in+person'
+curl -X POST 'http://localhost:8080/queue?name=12016768942&description=show+up+in+person'
 curl -X POST 'http://localhost:8080/queue?name=online&description=call'
-curl -X POST http://localhost:8080/queue/local -d who=mario
-curl -X POST http://localhost:8080/queue/local -d who=luigi
-curl -X POST http://localhost:8080/queue/local -d who=peach
-curl -X POST http://localhost:8080/queue/local -d who=toad
+curl -X POST http://localhost:8080/queue/12016768942 -d who=mario
+curl -X POST http://localhost:8080/queue/12016768942 -d who=luigi
+curl -X POST http://localhost:8080/queue/12016768942 -d who=peach
+curl -X POST http://localhost:8080/queue/12016768942 -d who=toad
 curl -X POST http://localhost:8080/queue/online -d who=toad
 curl -X POST http://localhost:8080/queue/online -d who=mario
 curl -X POST http://localhost:8080/queue/online -d who=wario
