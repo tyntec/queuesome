9ec29822bd533e1f3ad4ff8a6fcaa2da78e5b3ac
Sven Neuhaus <neuhaus@tyntec.com>
2017-09-30 14:43:51 +0200

    real queue names

--- provision-queues.sh
+++ provision-queues.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-curl -X POST 'http://localhost:8080/queue?name=test&description=test+1'
-curl -X POST 'http://localhost:8080/queue?name=test2&description=test+2'
-curl -X POST http://localhost:8080/queue/test?who=mario
-curl -X POST http://localhost:8080/queue/test?who=luigi
-curl -X POST http://localhost:8080/queue/test?who=peach
-curl -X POST http://localhost:8080/queue/test?who=toad
-curl -X POST http://localhost:8080/queue/test2?who=toad
-curl -X POST http://localhost:8080/queue/test2?who=mario
+curl -X POST 'http://localhost:8080/queue?name=local&description=show+up+in+person'
+curl -X POST 'http://localhost:8080/queue?name=online&description=call'
+curl -X POST http://localhost:8080/queue/local?who=mario
+curl -X POST http://localhost:8080/queue/local?who=luigi
+curl -X POST http://localhost:8080/queue/local?who=peach
+curl -X POST http://localhost:8080/queue/local?who=toad
+curl -X POST http://localhost:8080/queue/online?who=toad
+curl -X POST http://localhost:8080/queue/online?who=mario
 
 if type -f pretty-json 2> /dev/null ; then
-	curl -X POST http://localhost:8080/queue | pretty-json
+	curl http://localhost:8080/queue | pretty-json
 else
-	curl -X POST http://localhost:8080/queue
+	curl http://localhost:8080/queue
 fi
 
