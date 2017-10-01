dba6b7838048c27d3b7288016afb10c5a04863d8
Alexandros Touloupis <touloupis@tyntec.com>
2017-10-01 12:34:22 +0200

    Fixing voice

--- queue/src/main/java/com/tyntec/queuesome/queue/service/VoiceService.java
+++ queue/src/main/java/com/tyntec/queuesome/queue/service/VoiceService.java
@@ -7,6 +7,7 @@ import com.tyntec.queuesome.queue.repository.QueueBackendService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,39 +27,54 @@ public class VoiceService {
     PassphraseProvider passphrase;
 
     @RequestMapping(name = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_XML_VALUE)
-    public String getVoice(@RequestParam("From") String from, @RequestParam("To") String to, @RequestParam("CallSid") String callSid,
-                           @RequestParam("AccountSid") String accountSid, @RequestParam("CallStatus") String callStatus,
-                           @RequestParam("ApiVersion") String apiVersion, @RequestParam("CallerName") String callerName,
-                           @RequestParam("Direction") String direction) {
-        log.info("Got inbound call : {} From: {} To: {}, Direction: {}", from, to, direction);
+    public ResponseEntity<String> getVoice(@RequestParam(name = "From", required = false) String from,
+                                           @RequestParam(name = "To", required = false) String to,
+                                           @RequestParam(name = "CallSid", required = false) String callSid,
+                                           @RequestParam(name = "AccountSid", required = false) String accountSid,
+                                           @RequestParam(name = "CallStatus", required = false) String callStatus,
+                                           @RequestParam(name = "ApiVersion", required = false) String apiVersion,
+                                           @RequestParam(name = "CallerName", required = false) String callerName,
+                                           @RequestParam(name = "Direction", required = false) String direction) throws Exception {
 
-        List<QueueTicketEntity> queueTicketEntities = qSvc.queryQueue(from);
-        QueueTicketEntity queueTicketEntity = null;
-        for (QueueTicketEntity q : queueTicketEntities) {
-            if (q.getQueueName().equalsIgnoreCase(to)) {
-                queueTicketEntity = q;
-                break;
+        log.info("Got inbound call From: {} To: {}, Direction: {}. Call status: {}", from, to, direction, callStatus);
+
+        if(callStatus.equalsIgnoreCase("initiated") || callStatus.equalsIgnoreCase("in-progress")) {
+
+            to = to.replaceFirst("\\+", "").trim();
+            log.info("Queue name >{}<", to);
+            List<QueueTicketEntity> queueTicketEntities = qSvc.queryQueue(from);
+            QueueTicketEntity queueTicketEntity = null;
+            for (QueueTicketEntity q : queueTicketEntities) {
+                if (q.getQueueName().equalsIgnoreCase(to)) {
+                    queueTicketEntity = q;
+                    break;
+                }
             }
+            String response = null;
+            if (queueTicketEntity == null) {
+                queueTicketEntity = qSvc.enQueue(to, from);
+                response = createTtsResponse("Your ticket number is " + queueTicketEntity.getNumber() +
+                        ". Passphrase " + passphrase.generatePassphrase() + "." + getEstimationText(queueTicketEntity));
+            } else {
+                QueueEntity queue = qSvc.getQueue(to);
+                response = createTtsResponse("Appointment is already booked. Your ticket number is " + queueTicketEntity.getNumber()
+                        + ". There are " + (queue.getQueue().size() - 1) + " waiting in front of you." + getEstimationText(queueTicketEntity));
+            }
+
+            log.info("Response: {}", response);
+            return ResponseEntity.ok(response);
         }
-        if (queueTicketEntity == null) {
-            queueTicketEntity = qSvc.enQueue(to, from);
-            return createTtsResponse("Your ticket number is " + queueTicketEntity.getNumber() +
-                    ". Passphrase " + passphrase + "." + getEstimationText(queueTicketEntity));
-        } else {
-            QueueEntity queue = qSvc.getQueue(to);
-            return createTtsResponse("Appointment is already booked. Your ticket number is " + queueTicketEntity.getNumber()
-                    + ". There are " + (queue.getQueue().size() - 1) + " waiting in front of you." + getEstimationText(queueTicketEntity));
-        }
+        return ResponseEntity.ok(null);
 
     }
-    
+
     private String getEstimationText(QueueTicketEntity ticket) {
         if (ticket == null) return "Ticket not found.";
-    	int position = qSvc.getTicketPosition(ticket.getQueueName(), ticket.getNumber());
-    	String estimate = qSvc.estimate(position);
+        int position = qSvc.getTicketPosition(ticket.getQueueName(), ticket.getNumber());
+        String estimate = qSvc.estimate(position);
         return " Estimated time remaining: " + estimate;
     }
-    
+
     private String createTtsResponse(String text) {
         return "<Response><Say>" + text + "</Say></Response>";
     }
